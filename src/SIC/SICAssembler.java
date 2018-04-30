7c0ccfa5187c9b22295822b137c9ee7f0acdbc04
Steven Emmanuel <ksteven925@gmail.com>
2018-05-01 03:27:44 +0530

    minor refactoring

--- src/SIC/SICAssembler.java
+++ src/SIC/SICAssembler.java
@@ -5,9 +5,9 @@ public class SICAssembler {
         
         //Replace the below file locations with your own text file locations.
         
-        Input.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\Files\\SICInput.txt");   //getting SIC assembly program.
-        OpTab.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\Files\\Optab.txt");      //getting the operator table.
-        SymTab.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\Files\\Symtab.txt");    //getting the location for storing symbol table.
-        Result.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\Files\\Result.txt");    //getting the location for storing the intermediate object code
+        Input.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\TextFiles\\SICInput.txt");   //getting SIC assembly program.
+        OpTab.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\TextFiles\\Optab.txt");      //getting the operator table.
+        SymTab.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\TextFiles\\Symtab.txt");    //getting the location for storing symbol table.
+        Result.Start("D:\\collegeDocuments\\LabExperiments\\GitHub-Projects\\SINGLE PASS ASSEMBLER FOR SIC\\SINGLE-PASS-ASSEMBLER-FOR-SIC\\src\\TextFiles\\Result.txt");    //getting the location for storing the intermediate object code
     }
 }
